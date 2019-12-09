81477da6427185b5845428b3db931ef5a68f298e
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-12-09 22:42:56 +0800

    REFACTOR:Think I finalize the components restructuring

---
+++ src/Books.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react";
+import BookSelector from "./BookSelector";
+
+const Books = props => {
+  const { books } = props;
+  console.log(books);
+  return (
+    <Fragment>
+      {books.map(book => {
+        return (
+          <li key={book.title}>
+            <div className="book">
+              <div className="book-top">
+                <div
+                  className="book-cover"
+                  style={{
+                    width: 128,
+                    height: 192,
+                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                  }}
+                ></div>
+                <BookSelector />
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors}</div>
+            </div>
+          </li>
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default Books;
